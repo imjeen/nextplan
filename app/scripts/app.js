cb9b399b6e0d5c545008898205b31854c1a40263
imjeen <imjeen@sina.com>
2015-10-26 22:58:17 +0800

    mod home.html for UI

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,13 @@
 
 var app = angular.module('app',['ngMaterial']);
 
+/* material theme*/
+// app.config(function($mdThemingProvider) {
+//   $mdThemingProvider.theme('default')
+//     .primaryPalette('pink')
+//     .accentPalette('orange');
+// });
+
 app.controller('AppCtrl', ['$scope', function($scope){
 	$scope.title = 'it is a home page';
 }]);
