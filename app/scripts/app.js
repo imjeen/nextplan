a7446e124cb1c5af59839e6935126733043d7c79
imjeen <imjeen@sina.com>
2015-11-01 22:47:37 +0800

    add side nav

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,19 @@
 
-var app = angular.module('app',['ngMaterial']);
+var app = angular.module('app',['ngMaterial','ngMdIcons']);
 
 /* material theme*/
-// app.config(function($mdThemingProvider) {
-//   $mdThemingProvider.theme('default')
-//     .primaryPalette('pink')
-//     .accentPalette('orange');
-// });
+app.config(function ($mdThemingProvider) {
+     
+    $mdThemingProvider.theme('purple')
+        .primaryPalette('deep-purple', {
+            'default': '500',
+            
+        });
+ 
+    $mdThemingProvider.setDefaultTheme('purple');
+      
+});   
 
 app.controller('AppCtrl', ['$scope', function($scope){
 	$scope.title = 'it is a home page';
 }]);
-
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,22 @@
 
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var sass = require('gulp-sass');
 
 gulp.task('connect',function(){
 	connect.server();
+});
+
+gulp.task('sass',function(){
+	gulp.src('./app/styles/main.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./app/styles/'));
+});
+
+gulp.task('watch',function(){
+	// sass file
+	gulp.watch('./app/styles/**/*.scss',['sass'])
+		.on('change',function(e){
+			console.log('file ' + e.path + ' was ' + e.type + ', running tasks...');
+		});
 });
